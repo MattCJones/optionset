6e6be905b61ba05f1791bb9b3726cf664d22efa8
Matthew C Jones <matt.c.jones.aoe@gmail.com>
2020-08-20 21:56:20 -0400

    Progress in unit tests

--- unittests/runtests.py
+++ unittests/runtests.py
@@ -7,7 +7,8 @@ import re
 import subprocess
 import shlex
 
-RUNAPP = "../bin/optionset.py" # run the application
+RUNAPP = "../bin/optionset.py"  # run the application
+
 
 def test_re(regex, strToSearch):
     """Test that regex expression works for given strToSearch"""
@@ -16,11 +17,23 @@ def test_re(regex, strToSearch):
     else:
         return False
 
+
 def run_cmd(cmdStr):
     """Run a command and return the output"""
-    subproc = subprocess.run(shlex.split(cmdStr), capture_output=True)
+    subproc = subprocess.run(shlex.split(cmdStr), capture_output=True,
+            check=True)
     outStr = subproc.stdout.decode('UTF-8')
     return outStr, subproc.returncode
+
+
+def set_default_options():
+    """Set default options"""
+    defOptionStrs = ("@valid0Dir a", "@validExtension dat",
+            "@validFileBaseDir a", "@validCommentType Bash",
+            "@validFormat difficultPlacement",)
+    for defOptionStr in defOptionStrs:
+        _, _ = run_cmd(f"{RUNAPP} {defOptionStr}")
+
 
 class TestIO(unittest.TestCase):
     """Test input and output"""
@@ -45,26 +58,47 @@ class TestIO(unittest.TestCase):
 
     def test_extensions(self):
         """Test various valid file extensions"""
-        outputStr, _ = run_cmd(f"{RUNAPP} -a")
-        extensionStrs = ('dat', 'nml', 'none', 'txt', 'org', 'orig', 'yaml',)
-        for extensionStr in extensionStrs:
-            reHasExtension = re.compile(f".*{extensionStr}.*")
+        outputStr, _ = run_cmd(f"{RUNAPP} -a @validExtension")
+        settingStrs = ('dat', 'nml', 'none', 'txt', 'org', 'orig', 'yaml',)
+        for settingStr in settingStrs:
+            reHasExtension = re.compile(f".*{settingStr}.*")
             self.assertTrue(test_re(reHasExtension, outputStr))
+
+
+class TestInvalid(unittest.TestCase):
+    """Test invalid options"""
+    def setUp(self):
+        pass
+
+    def test_invalid(self):
+        """Test invalid files and directories"""
+        reInvalid = re.compile(".*ERROR.*")
+        outputStr, _ = run_cmd(f"{RUNAPP} -a")
+        self.assertFalse(test_re(reInvalid, outputStr))
 
 class TestValid(unittest.TestCase):
     """Test valid options"""
     def setUp(self):
         pass
 
-    def test_extensions(self):
-        """Test various valid file extensions"""
-        outputStr, _ = run_cmd(f"{RUNAPP} -a")
-        extensionStrs = ('dat', 'nml', 'none', 'txt', 'org', 'orig', 'yaml',)
-        for extensionStr in extensionStrs:
-            reHasExtension = re.compile(f".*{extensionStr}.*")
-            self.assertTrue(test_re(reHasExtension, outputStr))
+    def test_comment_types(self):
+        """Test various comment types"""
+        outputStr, _ = run_cmd(f"{RUNAPP} -a @validCommentType")
+        settingStr = ('Bash', 'CPP', 'MATLAB', 'NML', 'custom',)
+        for settingStr in settingStr:
+            reCommentType = re.compile(f".*{settingStr}.*")
+            self.assertTrue(test_re(reCommentType, outputStr))
+
+    def test_format(self):
+        """Test proper format of options and settings"""
+        outputStr, _ = run_cmd(f"{RUNAPP} -a @validFormat")
+        settingStr = ('difficultPlacement', 'multiline',)
+        for settingStr in settingStr:
+            reFormat = re.compile(f".*{settingStr}.*")
+            self.assertTrue(test_re(reFormat, outputStr))
 
 
 
 if __name__ == '__main__':
+    set_default_options()
     unittest.main()
